687c4c4372f9e762fb5a8126592d5982412ba52e
Bernard Moy <bernardmoy3@gmail.com>
2026-05-08 14:30:03 +0100

    The decomposer now accepts existing modules, and only output new ones

--- prompts/decomposer.py
+++ prompts/decomposer.py
@@ -1,6 +1,6 @@
 from .json_helper import get_json_string
 
-def get_decomposer_prompt(issue_title, issue_body, working_dir): 
+def get_decomposer_prompt(issue_title, issue_body, working_dir, related_modules): 
     return f"""
 You are a senior software engineer that specialises in modular software design.
 
@@ -9,9 +9,12 @@ Issue title: {issue_title}
 Issue body: {issue_body}
 Working directory: {working_dir}
 
+You are also given a list of related modules below, prioritise reusing them instead of creating a new module where possible: 
+{related_modules}
+
 Propose a modular design, each with a single responsibility, that when integrated together, achieve the goal specified in the issue.
 
-Output strictly a JSON object using the following schema.
+Output a JSON object including only the newly proposed modules, using the following schema. 
 
 {get_json_string("decomposer")}
 
