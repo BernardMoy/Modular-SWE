685f5fb5fcd7791b2c6a3638d8fe7276a986bfdf
BernardMoy <bernardmoy3@gmail.com>
2026-06-20 14:21:09 +0100

    bfs file now consider the types of modules

--- deterministic/helpers/deps_graph/bfs.py
+++ deterministic/helpers/deps_graph/bfs.py
@@ -2,15 +2,20 @@ import json
 import argparse 
 from ...reusables.get_all_modules import get_all_modules
 
-def bfs(deps_graph_json): 
+def bfs(deps_graph_json, design_json): 
     """
     This assumes the dependency graph has no cycles. 
+    This also assumes the design json strictly follow the rules below for keep, changed or new modules: 
+    (1) HAS CONCRETE IMPLEMENTATION? --> YES GOTO (2) NO 'new' 
+    (2) CHANGED? --> YES 'changed' NO 'keep' 
+    Modules not found in the design would be treated as new. 
 
     Return an array of array, each containing modules on a layer
     that can be implemented in parallel. 
     The implementation starts from the layer with no dependencies (out degree = 0)  
     """
     all_modules = get_all_modules(deps_graph_json)
+    
     visited = set()
     s = set(all_modules)
 
@@ -32,21 +37,36 @@ def bfs(deps_graph_json):
             visited.add(x)
 
         modules.append(cur)
-    return modules 
+    
+    # filter all_modules by only keeping the new or changed modules that need to be implemented 
+    # do this at the end to ensure all bfs paths are covered first 
+    types = {} 
+    for module in design_json: 
+        types[module["module_path"]] = module["type"]
+    
+    for i, module_list in enumerate(modules): 
+        modules[i] = [x for x in module_list if types[x] in ["changed", "new"]]
+    
+    # remove all empty arrays after doing this 
+    return [x for x in modules if len(x) > 0]
 
 
 
-# Usage: bfs [deps-graph-path] 
+# Usage: bfs [deps-graph-path] [design-path]
 def main(): 
     parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="Abs path to the dependency graph JSON file")
+    parser.add_argument("deps_graph_path", help="Abs path to the dependency graph JSON file")
+    parser.add_argument("design_path", help="Abs path to the design JSON file")
     args = parser.parse_args()
 
     # Read the JSON graph from the path 
-    with open(args.path, 'r') as f: 
+    with open(args.deps_graph_path, 'r') as f: 
         graph_json = json.load(f)
+    
+    with open(args.design_path, 'r') as f: 
+        design_json = json.load(f)
 
-    result = bfs(graph_json) 
+    result = bfs(graph_json, design_json) 
     print(result) 
 
 
