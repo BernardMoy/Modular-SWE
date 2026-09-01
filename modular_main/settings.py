3a82437cae942e6983d0d771dc9b591ec336bfc9
BernardMoy <bernardmoy3@gmail.com>
2026-09-01 18:56:38 +0100

    Script to write a pre generated reference metrics json

--- modular_main/settings.py
+++ modular_main/settings.py
@@ -15,8 +15,8 @@ MODEL: Literal[
 # auto: D-A loop analyzer makes suggestions automatically using the agent's thinking; no human is involved.
 # human: D-A loop analyzer would ask human questions if it encounters ambiguity / contextual uncertainty / contrasting metrics.
 WORKFLOW_MODE: Literal[
-    "noDesign", "autoNoMetric", "auto", "human", "autoByLayer"  # USED ONLY [4] noDesign, auto, autoNoMetric, autoByLayer   #  "autoTest", "autoAggressive", 
-] = "auto"
+    "noDesign", "autoNoMetric", "auto", "human", "autoByLayer", "autoTest"  # USED ONLY [4] noDesign, auto, autoNoMetric, autoByLayer   # "autoAggressive", 
+] = "autoTest"
 
 # Unused:
 # allAtOnce: DA loop, then implement all modules at once following the design
