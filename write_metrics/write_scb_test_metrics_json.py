5cd471b46382725a7db6def098a76f948b91a991
BernardMoy <bernardmoy3@gmail.com>
2026-09-07 13:02:41 +0100

    updated metrics cache

--- write_metrics/write_scb_test_metrics_json.py
+++ write_metrics/write_scb_test_metrics_json.py
@@ -22,7 +22,7 @@ from scripts.pytest_scb import pytest_scb
 # For simplicity, the keys used are simply the implementation folders
 SCB_DIR = Path("datasets/slopCodeBench/scb-problems")
 PROBLEMS = [
-    "sheeteval", "sith"
+     "code_search", "circuit_eval", "etl_pipeline", "meshctl", "mocked_http", "recli", "sheeteval", "sith"
     ]
 
 
